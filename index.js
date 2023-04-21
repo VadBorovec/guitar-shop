20564d3681e54bae76c1ea45146add87e40de471
VadBorovec <112804701+VadBorovec@users.noreply.github.com>
2023-04-21 04:55:56 +0300

    finish task

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ function render() {
   productsPage.render();
 }
 
-render();
+spinnerPage.render();
 
 let CATALOG = [];
+
+// server/catalog.json
+// https://api.jsonserve.com/bE9ZQP
+
+fetch("server/catalog.json")
+  .then((res) => res.json())
+  .then((body) => {
+    CATALOG = body;
+
+    setTimeout(() => {
+      spinnerPage.handleClear();
+      render();
+    }, 1000);
+  })
+  .catch(() => {
+    spinnerPage.handleClear();
+    errorPage.render();
+  });
